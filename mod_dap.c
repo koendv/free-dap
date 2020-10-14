c91559adc0dc66dbf0774b5e3e3401eda0c4f853
Koen De Vleeschauwer <kdv@kdvelectronics.eu>
2020-10-14 10:46:23 +0200

    dap.process(): execute dap requests from micropython scripts

--- mod_dap.c
+++ mod_dap.c
@@ -14,6 +14,7 @@
 STATIC const char dap_help_text[] =
     "dap.init()   -- start dap usb device\n"
     "dap.deinit() -- stop dap usb device\n"
+    "dap.process(req, resp) -- process dap request\n"
     "dap.hid_info -- usb hid descriptor for usb_mode()\n"
     "dap.calibrate() -- calibrate swd clock frequency\n";
 
@@ -24,12 +25,14 @@ int dap_config_delay_constant = DAP_DEFAULT_DELAY_CONSTANT;
 STATIC mp_obj_t mp_dap_help();
 STATIC mp_obj_t mp_dap_init();
 STATIC mp_obj_t mp_dap_deinit();
+STATIC mp_obj_t mp_dap_process(mp_obj_t req, mp_obj_t resp);
 STATIC mp_obj_t mp_dap_calibrate();
 STATIC const mp_rom_obj_tuple_t mp_dap_hidinfo_obj;
 
 STATIC MP_DEFINE_CONST_FUN_OBJ_0(mp_dap_help_obj, mp_dap_help);
 STATIC MP_DEFINE_CONST_FUN_OBJ_0(mp_dap_init_obj, mp_dap_init);
 STATIC MP_DEFINE_CONST_FUN_OBJ_0(mp_dap_deinit_obj, mp_dap_deinit);
+STATIC MP_DEFINE_CONST_FUN_OBJ_2(mp_dap_process_obj, mp_dap_process);
 STATIC MP_DEFINE_CONST_FUN_OBJ_KW(mp_dap_calibrate_obj, 0, mp_dap_calibrate);
 
 STATIC const mp_rom_map_elem_t dap_module_globals_table[] = {
@@ -38,6 +41,7 @@ STATIC const mp_rom_map_elem_t dap_module_globals_table[] = {
     {MP_ROM_QSTR(MP_QSTR_init), MP_ROM_PTR(&mp_dap_init_obj)},
     {MP_ROM_QSTR(MP_QSTR_deinit), MP_ROM_PTR(&mp_dap_deinit_obj)},
     {MP_ROM_QSTR(MP_QSTR_calibrate), MP_ROM_PTR(&mp_dap_calibrate_obj)},
+    {MP_ROM_QSTR(MP_QSTR_process), MP_ROM_PTR(&mp_dap_process_obj)},
     {MP_ROM_QSTR(MP_QSTR_hid_info), MP_ROM_PTR(&mp_dap_hidinfo_obj)},
 };
 
@@ -152,6 +156,62 @@ STATIC mp_obj_t mp_dap_calibrate(size_t n_args, const mp_obj_t *pos_args,
   cal_tup->items[0] = mp_obj_new_int(dap_config_delay_constant);
   cal_tup->items[1] = mp_obj_new_int(dap_config_fast_clock);
   return cal_obj;
+}
+
+/* access to bytes/bytearray contents and length */
+
+/*
+   o: micropython string, bytes or bytearray (input)
+   len: length of string, bytes or bytearray (output)
+   items: output, string, bytes or bytearray data (output)
+ */
+
+STATIC void mp_obj_get_data(mp_obj_t o, size_t *len, uint8_t **items) {
+  if (mp_obj_is_type(MP_OBJ_FROM_PTR(o), &mp_type_bytearray)) {
+    mp_obj_array_t *barray = MP_OBJ_FROM_PTR(o);
+    *len = barray->len;
+    *items = barray->items;
+    return;
+  }
+  if (mp_obj_is_type(MP_OBJ_FROM_PTR(o), &mp_type_bytes) ||
+      mp_obj_is_type(MP_OBJ_FROM_PTR(o), &mp_type_str)) {
+    mp_obj_str_t *str = MP_OBJ_FROM_PTR(o);
+    *len = str->len;
+    *items = (void *)str->data;
+    return;
+  }
+  mp_raise_TypeError(MP_ERROR_TEXT("expected string, bytes or bytearray"));
+}
+
+/* call dap from micropython.
+   arguments:
+     - request: string, bytes, or bytearray (64 bytes)
+     - response: string, bytes, or bytearray (64 bytes)
+   returns true if response needs to be sent; false if response does not need to
+   be sent.
+*/
+
+STATIC mp_obj_t mp_dap_process(mp_obj_t req, mp_obj_t resp) {
+  size_t app_request_len;
+  uint8_t *app_request_buffer;
+  size_t app_response_len;
+  uint8_t *app_response_buffer;
+
+  mp_obj_get_data(req, &app_request_len, &app_request_buffer);
+  if (app_request_len < DAP_CONFIG_PACKET_SIZE)
+    mp_raise_ValueError(MP_ERROR_TEXT("request too small"));
+
+  mp_obj_get_data(resp, &app_response_len, &app_response_buffer);
+  if (app_response_len < DAP_CONFIG_PACKET_SIZE)
+    mp_raise_ValueError(MP_ERROR_TEXT("response too small"));
+
+  if (dap_filter_request(app_request_buffer)) {
+    dap_process_request(app_request_buffer, app_response_buffer);
+    return mp_const_true;
+  }
+
+  memset(app_response_buffer, 0, DAP_CONFIG_PACKET_SIZE);
+  return mp_const_false;
 }
 
 // not truncated
